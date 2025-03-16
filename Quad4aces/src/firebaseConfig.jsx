1d55e5529c3b89588b1abe6e0ef42b3122157cfb
achyu-dev <achyugeorge7744@gmail.com>
2025-03-16 11:47:30 +0530

    ready code

--- Quad4aces/src/firebaseConfig.jsx
+++ Quad4aces/src/firebaseConfig.jsx
@@ -1,17 +1,15 @@
-// Import the functions you need from the SDKs you ne
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDYlbUkiQhlvaGDphiB9VZLPHrrJ6jNx60",
-  authDomain: "exam-proctor-58e4c.firebaseapp.com",
-  projectId: "exam-proctor-58e4c",
-  storageBucket: "exam-proctor-58e4c.firebasestorage.app",
-  messagingSenderId: "942076339924",
-  appId: "1:942076339924:web:31d05c703655171c8bbd41",
-  measurementId: "G-KLECNHJLTN"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
-
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -19,4 +17,3 @@ const db = getFirestore(app);
 
 export { app, db };
 export default firebaseConfig;
-
